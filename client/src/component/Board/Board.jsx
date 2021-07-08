63dd4484e5a306537976e3bd249b90ffda3ffe71
cristian-hr <cristian.hrauh@gmail.com>
2021-07-08 14:58:18 -0400

    sweetalert2 added

--- client/src/component/Board/Board.jsx
+++ client/src/component/Board/Board.jsx
@@ -1,61 +1,56 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import Square from '../Square/Square';
-import usePlays from "../../services/usePlays"
-import "./Board.css"
+import usePlays from "../../services/usePlays";
+import Swal from "sweetalert2";
+import "./Board.css";
 
 function Board(props) {
 
     const { roomId } = props.match.params;
     const { play, sendPlay } = usePlays(roomId);
-    
-    const [playerMark , setPlayerMark] = useState("")
+    const [player, setPlayer] = useState({ mark: "", won: false })
 
-    useEffect(()=> {
-        if (play.plays.length === 1 && playerMark === "") setPlayerMark("O")
-        if (play.plays.length === 0 && playerMark !== "") setPlayerMark("")
-    },[play]) 
+    useEffect(() => {
+        if (play.plays.length === 1 && player.mark === "") setPlayer({ ...player, mark: "O" })
+        if (play.plays.length === 0 && player.mark !== "") setPlayer({ ...player, mark: "" })
+        // eslint-disable-next-line
+    }, [play])
 
     useEffect(() => {
         if (play.plays.filter(p => p.coord === "1a" || p.coord === "1b" || p.coord === "1c").length === 3) {
             let marks = play.plays.filter(p => p.coord === "1a" || p.coord === "1b" || p.coord === "1c").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "2a" || p.coord === "2b" || p.coord === "2c").length === 3) {
             let marks = play.plays.filter(p => p.coord === "2a" || p.coord === "2b" || p.coord === "2c").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "3a" || p.coord === "3b" || p.coord === "3c").length === 3) {
             let marks = play.plays.filter(p => p.coord === "3a" || p.coord === "3b" || p.coord === "3c").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "1a" || p.coord === "2a" || p.coord === "3a").length === 3) {
             let marks = play.plays.filter(p => p.coord === "1a" || p.coord === "2a" || p.coord === "3a").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "1b" || p.coord === "2b" || p.coord === "3b").length === 3) {
             let marks = play.plays.filter(p => p.coord === "1b" || p.coord === "2b" || p.coord === "3b").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "1c" || p.coord === "2c" || p.coord === "3c").length === 3) {
             let marks = play.plays.filter(p => p.coord === "1c" || p.coord === "2c" || p.coord === "3c").map(pp => pp.mark)
             if (marks.every(m => m === marks[0])) {
-                alert(`Player ${marks[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+                setPlayer({ ...player, won: marks[0] })
             }
         }
         if (play.plays.filter(p => p.coord === "1a" || p.coord === "2b" || p.coord === "3c").length === 3 ||
@@ -65,14 +60,12 @@ function Board(props) {
             let marks2 = play.plays.filter(p => p.coord === "3a" || p.coord === "2b" || p.coord === "1c").map(pp => pp.mark)
 
             if (marks1.every(m => m === marks1[0]) && marks1.length === 3) {
-                
-                alert(`Player ${marks1[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+
+                setPlayer({ ...player, won: marks1[0] })
             }
             else if (marks2.every(m => m === marks2[0]) && marks2.length === 3) {
-                
-                alert(`Player ${marks2[0]} won`)
-                sendPlay({turn: "X", plays:[]})
+
+                setPlayer({ ...player, won: marks2[0] })
             }
         }
         if (play.plays.length === 9) {
@@ -83,39 +76,79 @@ function Board(props) {
                 let marks2 = play.plays.filter(p => p.coord === "3a" || p.coord === "2b" || p.coord === "1c").map(pp => pp.mark)
 
                 if (!marks1.every(m => m === marks1[0]) && !marks2.every(m => m === marks2[0])) {
-                    alert(`Draw`)
-                    sendPlay({turn: "X", plays:[]})
+                    setPlayer({ ...player, won: "tie" })
                 }
             }
         }
-    // eslint-disable-next-line
+        // eslint-disable-next-line
     }, [play])
+
+    useEffect(() => {
+        if (player.won === "X" || player.won === "O") {
+            setPlayer({ mark: "", won: false })
+            Swal.fire({
+                title: `Player ${player.won} won`,
+                icon: "success",
+                confirmButtonColor: '#353535',
+                showCancelButton: true,
+                cancelButtonText: "Try again",
+                cancelButtonColor: '#008500',
+            })
+                .then((result) => {
+                    if (result.isDismissed) {                        
+                        sendPlay({ turn: "X", plays: [] })
+                    }
+                })
+                .catch((error) => {
+                    console.error(error)
+                })                        
+        }
+        else if (player.won === "tie") {
+            setPlayer({ mark: "", won: false })
+            Swal.fire({
+                title: `XO tie`,
+                icon: "success",
+                confirmButtonColor: '#353535',
+                showCancelButton: true,
+                cancelButtonText: "Try again",
+                cancelButtonColor: '#008500',
+            })
+                .then((result) => {
+                    if (result.isDismissed) {
+                        sendPlay({ turn: "X", plays: [] })
+                    }
+                })
+                .then(() => {
+                    if(!play.plays[0]) Swal.close()
+                })
+                .catch((error) => {
+                    console.error(error)
+                })
+        }
+        if(!play.plays[0]) Swal.close()     
+        // eslint-disable-next-line
+    }, [player, play])
 
     function handleClick(e, coord) {
         e.preventDefault()
 
         if (play.plays.length === 0) {
-            console.log("holaaaa")
-            setPlayerMark("X")
-            sendPlay({turn: "O", plays:[...play.plays,{ mark: "X", coord: coord }]})
+            setPlayer({ ...player, mark: "X" })
+            sendPlay({ turn: "O", plays: [...play.plays, { mark: "X", coord: coord }] })
         }
 
-        // if (play.plays.find(p => p.coord === coord)) {
-        //     alert("Select other square")
-        // }
-
-        else if (playerMark === "X" && play.plays.length%2 === 0) {
-            sendPlay({turn: "O", plays:[...play.plays,{ mark: "X", coord: coord }]})
+        else if (player.mark === "X" && play.plays.length % 2 === 0) {
+            sendPlay({ turn: "O", plays: [...play.plays, { mark: "X", coord: coord }] })
         }
-        else if (playerMark === "O" && play.plays.length%2 !== 0){
-            sendPlay({turn: "X", plays:[...play.plays,{ mark: "O", coord: coord }]})
+        else if (player.mark === "O" && play.plays.length % 2 !== 0) {
+            sendPlay({ turn: "X", plays: [...play.plays, { mark: "O", coord: coord }] })
         }
     }
 
     function reset(e) {
         e.preventDefault()
-        sendPlay({turn: "X", plays:[]})
-        setPlayerMark("")
+        sendPlay({ turn: "X", plays: [] })
+        setPlayer({ mark: "", won: false })
     }
 
     return (
